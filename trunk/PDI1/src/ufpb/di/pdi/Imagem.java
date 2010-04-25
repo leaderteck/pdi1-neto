abdeef02b2e811326fd25ce68f2ab4b3e5b5331d
juracylucena <juracylucena@dbe9fc52-8a83-a92e-78f2-356a3102c12d>
2010-04-25 15:48:35 +0000

    Conversões: Banda R, Banda G, Banda B e Negativo

--- trunk/PDI1/src/ufpb/di/pdi/Imagem.java
+++ trunk/PDI1/src/ufpb/di/pdi/Imagem.java
@@ -2,6 +2,10 @@ package ufpb.di.pdi;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -18,7 +22,22 @@ public class Imagem {
     }
 
     public Imagem(String caminhoArquivo) {
-        
+        try {
+            arquivo = new File(caminhoArquivo);
+            imagem = ImageIO.read(arquivo);
+        } catch (IOException ex) {
+            System.out.println("Erro na abertura do arquivo. Verifique a extensão. \n");
+            Logger.getLogger(Imagem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void salvarImagem(File arquivo) {
+        try {
+            ImageIO.write(getImagem(), "JPG", arquivo);
+        } catch (IOException ex) {
+            System.out.println("Não foi possível salvar o arquivo. \n");
+            Logger.getLogger(Imagem.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public File getArquivo() {
--- trunk/PDI1/src/ufpb/di/pdi/operacoes/Conversoes.java
+++ trunk/PDI1/src/ufpb/di/pdi/operacoes/Conversoes.java
@@ -1,31 +1,78 @@
 package ufpb.di.pdi.operacoes;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import ufpb.di.pdi.Imagem;
 
 /**
- *
- * @author neto
+ * Operações de Conversão em uma imagem RGB
+ * @since 23 de abril de 2010
+ * @author Juracy Neto - juracylucena[at]gmail.com
  */
 public class Conversoes {
 
-    public Imagem bandaR(Imagem imagemOriginal) {
-
-        return new Imagem(null, null);
+    public static Imagem bandaR(Imagem imagemOriginal, File novoArquivo) {
+        BufferedImage imagem = imagemOriginal.getImagem();
+        Integer altura = imagem.getHeight();
+        Integer largura = imagem.getWidth();
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                int rgb = imagem.getRGB(j, i);
+                int r = (rgb >> 16) & 255;
+                rgb = (r << 16) | 0 | 0;
+                imagem.setRGB(j, i, rgb);
+            }
+        }
+        return new Imagem(novoArquivo, imagem);
     }
 
-    public Imagem bandaG(Imagem imagemOriginal) {
-
-        return new Imagem(null, null);
+    public static Imagem bandaG(Imagem imagemOriginal, File novoArquivo) {
+        BufferedImage imagem = imagemOriginal.getImagem();
+        Integer altura = imagem.getHeight();
+        Integer largura = imagem.getWidth();
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                int rgb = imagem.getRGB(j, i);
+                int g = (rgb >> 8) & 255;
+                rgb = 0 | (g << 8) | 0;
+                imagem.setRGB(j, i, rgb);
+            }
+        }
+        return new Imagem(novoArquivo, imagem);
     }
 
-    public Imagem bandaB(Imagem imagemOriginal) {
-
-        return new Imagem(null, null);
+    public static Imagem bandaB(Imagem imagemOriginal, File novoArquivo) {
+        BufferedImage imagem = imagemOriginal.getImagem();
+        Integer altura = imagem.getHeight();
+        Integer largura = imagem.getWidth();
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                int rgb = imagem.getRGB(j, i);
+                int b = (rgb) & 255;
+                rgb = 0 | 0 | b;
+                imagem.setRGB(j, i, rgb);
+            }
+        }
+        return new Imagem(novoArquivo, imagem);
     }
 
-    public Imagem negativo(Imagem imagemOriginal) {
-
-        return new Imagem(null, null);
+    public static Imagem negativo(Imagem imagemOriginal, File novoArquivo) {
+        BufferedImage imagem = imagemOriginal.getImagem();
+        Integer altura = imagem.getHeight();
+        Integer largura = imagem.getWidth();
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                int rgb = imagem.getRGB(j, i);
+                int r = (rgb >> 16) & 255;
+                int g = (rgb >> 8) & 255;
+                int b = (rgb) & 255;
+                r = 255 - r;
+                g = 255 - g;
+                b = 255 - b;
+                rgb = (r >> 16) | (g << 8) | b;
+                imagem.setRGB(j, i, rgb);
+            }
+        }
+        return new Imagem(novoArquivo, imagem);
     }
-    
 }
