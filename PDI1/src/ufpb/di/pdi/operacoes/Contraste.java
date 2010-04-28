86826cb1eea3b6edf4d280009e4a5ff5d270b351
juracylucena <juracylucena@dbe9fc52-8a83-a92e-78f2-356a3102c12d>
2010-04-28 02:17:10 +0000

    Mediana Merge

---
+++ PDI1/src/ufpb/di/pdi/operacoes/Contraste.java
@@ -0,0 +1,19 @@
+package ufpb.di.pdi.operacoes;
+
+import java.io.File;
+import ufpb.di.pdi.Imagem;
+
+/**
+ * Operações de Filtagem em uma imagem RGB
+ * @since 23 de abril de 2010
+ * @author Juracy Neto - juracylucena[at]gmail.com
+ * @author Erison Moura - erisonmoura[at]gmail.com
+ */
+public class Contraste {
+
+     public static Imagem contrasteAdaptativo(Imagem imagemOriginal, File novoArquivo, int constante) {
+         
+         return null;
+     }
+
+}
