e83d4f388a8523f4e27df80cb20ab57ecb1bd5f5
juracylucena <juracylucena@dbe9fc52-8a83-a92e-78f2-356a3102c12d>
2010-04-29 14:58:41 +0000

    Final

--- PDI1/src/ufpb/di/pdi/operacoes/Filtros.java
+++ PDI1/src/ufpb/di/pdi/operacoes/Filtros.java
@@ -67,8 +67,6 @@ public class Filtros {
         int somaG = 0;
         int somaB = 0;
         int contador = 0;
-
-
         int limiteLateral = vizinhanca / 2;
         int limSuplinha, limInflinha, limSupColuna, limInfColuna;
         for (int j = 0; j < altura; j++) {
@@ -119,62 +117,107 @@ public class Filtros {
     }
 
     public static BufferedImage medianaVizinhanca(int vizinhanca, BufferedImage imagemOriginal, int[][][] matrizImagem) {
-        Integer altura = imagemOriginal.getHeight();
-        Integer largura = imagemOriginal.getWidth();
-        int[][][] matrizNovaImagem = new int[altura][largura][3];
-        int[] somaR = new int[altura * largura];
-        int[] somaG = new int[altura * largura];
-        int[] somaB = new int[altura * largura];
+        int altura = imagemOriginal.getHeight();
+        int largura = imagemOriginal.getWidth();
+        int[] vetorRGB = new int[altura * largura];
+        int[][][] matrizNovaImagem = new int[altura][largura][3];        
+        int valor1 = (int) Math.round(vizinhanca / 2);
         int contador = 0;
-        if (vizinhanca % 2 != 0) {
-            vizinhanca--;
-        }
-        int pivo = (altura * largura) / 2;
-        int limiteLateral = vizinhanca / 2;
-        int limSuplinha, limInflinha, limSupColuna, limInfColuna;
-        for (int j = 0; j < altura; j++) {
-            for (int i = 0; i < largura; i++) {
-                limInflinha = j - limiteLateral;
-                limSuplinha = j + limiteLateral;
-                limInfColuna = i - limiteLateral;
-                limSupColuna = i + limiteLateral;
-                for (; limInflinha <= limSuplinha; limInflinha++, limInfColuna = i - limiteLateral) {
-                    for (; limInfColuna <= limSupColuna; limInfColuna++) {
-                        if ((limInflinha >= 0) && (limInfColuna >= 0) && (limInflinha < j) && (limInfColuna < i)) {
-                            somaR[contador] = matrizImagem[limInflinha][limInfColuna][0];
-                            somaG[contador] = matrizImagem[limInflinha][limInfColuna][1];
-                            somaB[contador] = matrizImagem[limInflinha][limInfColuna][2];
-                            contador++;
+        int auxR = 0;
+        int auxG = 0;
+        int auxB = 0;
+        int pivot = (vizinhanca * vizinhanca) / 2;
+        int[] vetorR = new int[vizinhanca * vizinhanca];
+        int[] vetorG = new int[vizinhanca * vizinhanca];
+        int[] vetorB = new int[vizinhanca * vizinhanca];
+        if ((vizinhanca % 2) == 0) {
+            int valor2 = valor1 - 1;
+            for (int lin = 0; lin < altura; lin++) {
+                for (int col = 0; col < largura; col++) {
+                    int limInflinha = lin - valor2;
+                    int limSuplinha = lin + valor1;
+                    int limInfColuna = col - valor2;
+                    int limSupColuna = col + valor1;
+                    for (; limInflinha <= limSuplinha; limInflinha++, limInfColuna = col - valor2) {
+                        for (; limInfColuna <= limSupColuna; limInfColuna++) {
+                            if ((limInflinha >= 0) && (limInfColuna >= 0) && (limInflinha < altura) && (limInfColuna < largura)) {
+                                vetorR[contador] = matrizImagem[limInflinha][limInfColuna][0];
+                                vetorG[contador] = matrizImagem[limInflinha][limInfColuna][1];
+                                vetorB[contador] = matrizImagem[limInflinha][limInfColuna][2];
+                                contador++;
+                            }
                         }
-
                     }
+
+                    Arrays.sort(vetorR);
+                    Arrays.sort(vetorG);
+                    Arrays.sort(vetorB);
+
+                    auxR = (vetorR[pivot] + vetorR[pivot - 1]) / 2;
+                    auxG = (vetorG[pivot] + vetorG[pivot - 1]) / 2;
+                    auxB = (vetorB[pivot] + vetorB[pivot - 1]) / 2;
+
+
+                    matrizNovaImagem[lin][col][0] = auxR;
+                    matrizNovaImagem[lin][col][1] = auxG;
+                    matrizNovaImagem[lin][col][2] = auxB;
+
+                    contador = 0;
+
                 }
-                Arrays.sort(somaR);
-                Arrays.sort(somaG);
-                Arrays.sort(somaB);
+            }
+        } else {
+            for (int lin = 0; lin < altura; lin++) {
+                for (int col = 0; col < largura; col++) {
 
-                int medianaR = (somaR[pivo] + somaR[pivo - 1]) / 2;
-                int medianaG = (somaG[pivo] + somaG[pivo - 1]) / 2;
-                int medianaB = (somaB[pivo] + somaB[pivo - 1]) / 2;
+                    int limInflinha = lin - valor1;
+                    int limSuplinha = lin + valor1;
+                    int limInfColuna = col - valor1;
+                    int limSupColuna = col + valor1;
 
-                matrizNovaImagem[j][i][0] = medianaR;
-                matrizNovaImagem[j][i][1] = medianaG;
-                matrizNovaImagem[j][i][2] = medianaB;
+                    for (; limInflinha <= limSuplinha; limInflinha++, limInfColuna = col - valor1) {
+                        for (; limInfColuna <= limSupColuna; limInfColuna++) {
+                            if ((limInflinha >= 0) && (limInfColuna >= 0) && (limInflinha < altura) && (limInfColuna < largura)) {
+                                vetorR[contador] = matrizImagem[limInflinha][limInfColuna][0];
+                                vetorG[contador] = matrizImagem[limInflinha][limInfColuna][1];
+                                vetorB[contador] = matrizImagem[limInflinha][limInfColuna][2];
+                                contador++;
+                            }
+                        }
+                    }
 
-                contador = 0;
+                    Arrays.sort(vetorR);
+                    Arrays.sort(vetorG);
+                    Arrays.sort(vetorB);
+
+                    matrizNovaImagem[lin][col][0] = vetorR[pivot];
+                    matrizNovaImagem[lin][col][1] = vetorG[pivot];
+                    matrizNovaImagem[lin][col][2] = vetorB[pivot];
+
+                    contador = 0;
+
+                }
             }
         }
-        BufferedImage novaImagem = new BufferedImage(largura, altura, 1);
-        for (int j = 0; j < altura; j++) {
-            for (int i = 0; i < largura; i++) {
-                int r = ((matrizNovaImagem[j][i][0])) & 255;
-                int g = ((matrizNovaImagem[j][i][1])) & 255;
-                int b = (matrizNovaImagem[j][i][2]) & 255;
-                int rgb = (r << 16) | (g << 8) | (b);
-                novaImagem.setRGB(i, j, rgb);
+
+        for (int lin = 0; lin < altura; lin++) {
+            for (int col = 0; col < largura; col++) {
+                matrizImagem[lin][col][0] = matrizNovaImagem[lin][col][0];
+                matrizImagem[lin][col][1] = matrizNovaImagem[lin][col][1];
+                matrizImagem[lin][col][2] = matrizNovaImagem[lin][col][2];
             }
         }
 
-        return novaImagem;
+        for (int lin = 0; lin < altura; lin++) {
+            for (int col = 0; col < largura; col++) {
+                vetorRGB[largura * lin + col] = ((matrizImagem[lin][col][0]) << 16) | ((matrizImagem[lin][col][1]) << 8) | (matrizImagem[lin][col][2] << 0);
+            }
+        }
+
+        BufferedImage imagemMediana = new BufferedImage(altura, largura, 1);
+        imagemMediana.setRGB(0, 0, largura, altura, vetorRGB, 0, altura);
+        return imagemMediana;
+
     }
 }
+
